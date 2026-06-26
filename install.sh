d6e27da9bf2eb91ca193d4df2d1815d7ee1126ef
Vaynnore-r <268145842+Vaynnore-r@users.noreply.github.com>
2026-06-26 13:56:12 +0200

    update 1

--- install.sh
+++ install.sh
@@ -2,11 +2,19 @@
 read -p "Install Dark Triad Launcher? (y/n): " answer </dev/tty
 if [[ "$answer" =~ ^[Yy](es)?$ ]]; then
     echo "Proceeding..."
+    cd 
     mkdir -p ~/.DTL/{launcher,apps,custom}
     rm -rf ~/.DTL/temp
     git clone https://github.com/Vaynnore-r/DTL.git ~/.DTL/temp
-
-
+    clear
+    read -p "Add dtl as terminal commend ? (use it only once to prevent bugs) (y/n): " answer </dev/tty
+    if [[ "$answer" =~ ^[Yy](es)?$ ]]; then
+        echo "
+        ## DTL commands
+        alias dtl ='./~/.DTL/launcher/launcher.sh'
+        " >> ~/.bashrc
+        source .bashrc
+    fi
 else
     echo "Canceled."
     exit 0
